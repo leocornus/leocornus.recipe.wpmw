47daaa89f9226bcb0fb20ea2f13a5a4ecbf4071b
Sean Chen <sean.chen@leocorn.com>
2016-02-22 08:58:20 -0500

    fix the file name and add the manuel package for testing.

--- leocornus/recipe/wpmw/tests.py
+++ leocornus/recipe/wpmw/tests.py
@@ -20,7 +20,7 @@ def setUp(test):
 def test_suite():
     suite = unittest.TestSuite((
             doctest.DocFileSuite(
-                'README.txt',
+                'README.rst',
                 setUp=setUp,
                 tearDown=zc.buildout.testing.buildoutTearDown,
                 optionflags=optionflags,
